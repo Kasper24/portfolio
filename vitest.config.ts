33de7f61c6b6be871a96e600d17327e44c067925
Ofek Itscovits <ofek4430@gmail.com>
2025-02-13 13:50:33 +0200

    feat: add PR comment showing test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   },
   resolve: {
     alias: {
