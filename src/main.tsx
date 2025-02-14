164f3cabc818cde238ebbfb402523f48df5a8207
Ofek Itscovits <ofek4430@gmail.com>
2025-02-14 22:42:56 +0200

    refactor: move app.css to styles folder and rename to globals.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css";
+import "./styles/app.css";
 import App from "./app";
 
 createRoot(document.getElementById("root")!).render(
